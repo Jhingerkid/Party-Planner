43b66f784fa36fd131b6567ffb81d1801e3c09f7
Thomas Black <tomjblack250@gmail.com>
2021-05-08 15:37:35 -0700

    getting latest

--- main.js
+++ main.js
@@ -2,13 +2,17 @@ import { gatherMenuData } from "./API/gatherMenuData.js";
 import { display } from "./nav.js";
 import { createTotalModal } from "./Modal/createModal.js";
 
-// console.log(document.querySelector("#main-page"));
-
 display(document.querySelector("#main-page"));
 
-document.querySelector("#clickme").addEventListener("click", async function () {
-	gatherMenuData();
+document.querySelector(".restaurantOrder").addEventListener("click", async function () {
+	display("#restaurant-menu");
 });
+
+// I would like to be able to use display(gatherMenuData());
+document.querySelector("#clickme").addEventListener("click", async function () {
+	gatherMenuData("#restaurant-menu");
+});
+// overlay
 document.querySelector(".sidebar-button").addEventListener("click", function () {
 	createTotalModal();
 });
