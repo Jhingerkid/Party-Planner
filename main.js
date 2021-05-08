446c46e5e2c67c25cd611b8efc3e97d0ac0081a4
Jhingerkid <barr6592@gmail.com>
2021-05-07 19:04:43 -0700

    Modal Created
    The modal is created but we need to discuss what data will be passed to it

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 import { gatherMenuData } from "./API/gatherMenuData.js";
+import { createTotalModal } from "./Modal/createModal.js";
 
 document.querySelector("#clickme").addEventListener("click", function(){
     gatherMenuData();
+});
+
+document.querySelector(".sidebar-button").addEventListener("click", function (){
+    createTotalModal();
 });
